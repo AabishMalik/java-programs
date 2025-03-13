0708c294b38183b8a0b349486feff4108d0f454e
Aabish Malik <aabishmalik3337@gmail.com>
2025-03-13 14:09:47 +0530

    update

--- regular/Overriding.java
+++ regular/Overriding.java
@@ -5,13 +5,25 @@ class A{
     void display(){
         System.out.println("A class");
     }
+
+    Number add(int a, int b){
+        return a+b;
+    }
 }
 
 class B extends A{
+
+    // explicit overriding 
     @Override
     void display(){
         System.out.println("B class");
     }
+
+    // implicit overriding + overloading
+    float add(float a, float b){
+        return a+b;
+    }
+
 }
 
 
